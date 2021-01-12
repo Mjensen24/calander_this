981d94a638ae39f6791829dff54eafc0f34f0462
Elyse Steingold <elyselindsays@gmail.com>
2021-01-12 18:49:28 -0500

    testing connection

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,23 @@
 from flask import Blueprint, render_template
+import os
 
 bp = Blueprint('main', __name__, url_prefix='')
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 
 @bp.route('/')
-def test():
-    return render_template('main.html')
+def main():
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime
+            )
+            return render_template('main.html')
